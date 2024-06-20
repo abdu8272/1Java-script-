852f824cebabc75d8ba0109a0a143eb3fd8803cb
abdu <abdu>
2024-06-20 12:59:47 +0500

    Java script

---
+++ script.js
@@ -0,0 +1,13 @@
+let str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, maiores!";
+let count = 0;
+
+const arr = ['a', 'e', 'i', 'o', 'u'];
+
+
+for (let char of text.toLowerCase()) {
+    if (vowels.includes(char)) {
+        count++;
+    }
+}
+
+console.log(` matnida ${count} ta unli harf bor.`);
